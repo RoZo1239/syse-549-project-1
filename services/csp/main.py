8f52f644a870a26a0de3f8a551eda4a6ceec5c16
Jonathan Christensen <27783364+jonathan-christensen@users.noreply.github.com>
2026-09-13 12:58:12 -0600

    Subscriber + CSP

--- services/csp/main.py
+++ services/csp/main.py
@@ -2,18 +2,25 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import os
 import uvicorn
+import secrets
 from dotenv import load_dotenv
 
-from event import Event
-
-TEAM = os.getenv("TEAM")
-HOST = os.getenv("HOST", "127.0.0.1")
-PORT = int(os.getenv("CSP_PORT", "8000"))
-SERVICE = "csp"
+from shared.model import RunRequest, RunResponse, SubscriberResponse, Event, EventResponse, ApplicantRequest, ApplicantResponse, SubscriberRequest
+from shared.user_database import UserDatabase
 
 load_dotenv()
 
+SERVICE = "csp"
+TEAM = os.getenv("TEAM")
+HOST = os.getenv("HOST", "127.0.0.1")
+PORT = int(os.getenv("CSP_PORT", "8000"))
+BASE_URL = HOST + ":" + str(PORT)
+
+events = EventResponse()
+
 app = FastAPI(title=SERVICE)
+
+user_db = UserDatabase()
 
 @app.get("/health")
 async def health():
@@ -25,11 +32,37 @@ async def transcript():
 
 @app.post("/reset")
 async def reset():
+    user_db.reset()
+    events.reset()
     return JSONResponse(status_code=200, content={"status": "ok"})
 
+@app.post("/apply")
+async def run(body: ApplicantRequest):
+    token=secrets.token_urlsafe(32)
+
+    user_db.add_user(body.email, token)
+
+    return ApplicantResponse(
+        token=token
+    )
+
+@app.post("/subscribe")
+async def run(body: SubscriberRequest):
+    subscribed = user_db.subscribe_user(body.email, body.token)
+
+    status_code = subscribed and 200 or 400
+    response = SubscriberResponse(
+        status = subscribed and "ok" or "error"
+    )
+
+    return JSONResponse(
+        status_code=status_code,
+        content=response.model_dump()
+    )
+    
 if __name__ == "__main__":
     uvicorn.run(
-        "csp.main:app",
+        "services.csp.main:app",
         host=HOST,
         port=PORT,
         reload=True
